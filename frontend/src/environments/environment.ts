98a0f1bacac9e49aab6239f324334ff3fa2939d1
Diego Hdez <di_hernandez@uxmcc2.iimas.unam.mx>
2021-02-17 19:59:50 -0600

    Added environment values

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'coffe-shop-project-dhdez.us.auth0.com', // the auth0 domain prefix
+    audience: 'coffee-shop-api', // the audience set for the auth0 app
+    clientId: '54uVOEc5TsTt3emkI5RxA3HiQmsLheng', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
